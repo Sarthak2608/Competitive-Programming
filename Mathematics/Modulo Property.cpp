9349c6f0b12988c10123e0575128f1513fc33315
Sarthak2608 <48095368+Sarthak2608@users.noreply.github.com>
2020-01-27 17:07:38 +0530

    Create Modulo Property.cpp

---
+++ Mathematics/Modulo Property.cpp
@@ -0,0 +1,23 @@
+/*
+
+a%n=b%n 
+then largest value of n=b-a or n can be the divisors of (b-a)
+
+a%(2^n)= a&(n-1)
+
+
+Tips:
+
+/****************************************/
+(-3)%2=-1 
+3%2=1
+bool odd(int x)
+{
+  return x%2==-1||x%2==1;
+  //or
+  return x%2!=0
+}
+/****************************************/
+
+
+*/
