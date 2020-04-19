49a7fbfd3e8597926aa238f6771281f20c3f0b50
Sarthak Jain <48095368+Sarthak2608@users.noreply.github.com>
2020-04-19 15:53:56 +0530

    Create  Kth Largest Element in an Array.cpp

---
+++ Data Structure/Binary Search/Kth Largest Element in an Array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        long lw=INT_MIN;
+        long hg=INT_MAX;
+        while(lw<=hg)
+        {
+            long mid=(lw+hg)/2;
+            int ct=0,e=0;
+            for(auto i:nums)
+            {
+                if(i>mid)
+                    ct++;
+                else if(i==mid)
+                    e++;
+            }
+            if(ct<k&&ct+e>=k)
+            {
+                return mid;
+            }
+            else if(ct+e<k)
+            {
+                hg=mid-1;
+            }
+            else  
+            {
+                lw=mid+1;
+            }
+            
+        }
+        return 0;
+    }
+};
