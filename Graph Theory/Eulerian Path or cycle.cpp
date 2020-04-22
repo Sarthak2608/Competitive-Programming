1394f188b62977438c05d8cb0e44a86e3d3529bf
Sarthak Jain <48095368+Sarthak2608@users.noreply.github.com>
2020-04-23 00:21:08 +0530

    Create Eulerian Path or cycle.cpp

---
+++ Graph Theory/Eulerian Path or cycle.cpp
@@ -0,0 +1,12 @@
+/*
+Fact:
+
+Every undirected component has the two or zero odd degree vertices.
+IF all vertices has even degree then it is possible to find Eulerian Circuit( Or Euler Cycle) 
+
+
+youtube.com/watch?v=xR4sGgwtR2I
+https://www.youtube.com/watch?v=8MpoO2zA2l4
+https://codeforces.com/contest/292/problem/E
+https://codeforces.com/contest/292/submission/77671090
+*/
