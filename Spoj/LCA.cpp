108c8a016ba1cef7fd23ff0a05e30ef48763bc66
Sarthak Jain <48095368+Sarthak2608@users.noreply.github.com>
2020-06-01 04:00:35 +0530

    Update LCA.cpp

--- Spoj/LCA.cpp
+++ Spoj/LCA.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int n,u,x,v,n1;
 vector<vector<int>> adj,dp;  //dp[log2(n)][n]
-vector<int> visit,level,parent;
+vector<int> vis,level,parent;
 void dfs(int node,int l,int p)
 {
-    visit[node]=1;
+    vis[node]=1;
     level[node]=l;
     parent[node]=p;
     for(auto child:adj[node])
     {
-        if(!visit[child])
+        if(!vis[child])
         {
             dfs(child,l+1,node);
         }
@@ -62,39 +62,39 @@ void findCommonAncestor(int u,int v)
 }
 
 int main() {
-	int t,tt=1;
-	cin>>t;
-	while(t--)
-	{
-	    cout<<"Case "<<tt<<":"<<endl;
-	    tt++;
-	    cin>>n;
-	    n1=log2(n)+2;
-	    dp=vector<vector<int>>(n1+1,vector<int>(n+1));
-	    adj=vector<vector<int>>(n+1);
-	    visit=vector<int>(n+1);
-	    level=vector<int>(n+1);
-	    parent=vector<int>(n+1);
-	    for(int i=1;i<=n;i++)
-	    {
-	        cin>>x;
-	        for(int j=1;j<=x;j++)
-	        {
-	            cin>>v;
-	            adj[i].push_back(v);
-	            adj[v].push_back(i);
-	        }
-	    }
-	    
-	    dfs(1,0,0);
-	    findAncestors();
-	    int q;
-	    cin>>q;
-	    while(q--)
-	    {
-	        cin>>u>>v;
-	        findCommonAncestor(u,v);
-	    }
-	}
-	return 0;
+  int t,tt=1;
+  cin>>t;
+  while(t--)
+  {
+      cout<<"Case "<<tt<<":"<<endl;
+      tt++;
+      cin>>n;
+      n1=log2(n)+2;
+      dp=vector<vector<int>>(n1+1,vector<int>(n+1));
+      adj=vector<vector<int>>(n+1);
+      vis=vector<int>(n+1);
+      level=vector<int>(n+1);
+      parent=vector<int>(n+1);
+      for(int i=1;i<=n;i++)
+      {
+          cin>>x;
+          for(int j=1;j<=x;j++)
+          {
+              cin>>v;
+              adj[i].push_back(v);
+              adj[v].push_back(i);
+          }
+      }
+      
+      dfs(1,0,0);
+      findAncestors();
+      int q;
+      cin>>q;
+      while(q--)
+      {
+          cin>>u>>v;
+          findCommonAncestor(u,v);
+      }
+  }
+  return 0;
 }
