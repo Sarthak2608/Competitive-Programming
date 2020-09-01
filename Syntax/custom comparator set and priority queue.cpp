131fb4651e4c63e80ab8675a35e34357e84be5c3
Sarthak Jain <48095368+Sarthak2608@users.noreply.github.com>
2020-09-01 21:45:41 +0530

    Create custom comparator set and priority queue.cpp

---
+++ Syntax/custom comparator set and priority queue.cpp
@@ -0,0 +1,14 @@
+ /*
+ auto cmp=[&](int a,int b){
+    return a>b;
+  };
+ set<int,decltype(cmp)> s(cmp);
+ */
+  
+  
+ /*
+ auto cmp=[&](int a,int b){
+    return a<b;
+  };
+  priority_queue<int,vector<int>,decltype(cmp)> q(cmp);
+ */
