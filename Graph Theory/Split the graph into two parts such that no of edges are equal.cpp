7f095e5bc4feede278047b4a10524c7318b4c683
Sarthak Jain <48095368+Sarthak2608@users.noreply.github.com>
2020-09-30 01:04:27 +0530

    Create Split the graph into two parts such that no of edges are equal.cpp

---
+++ Graph Theory/Split the graph into two parts such that no of edges are equal.cpp
@@ -0,0 +1,10 @@
+/*
+Split the graph into two parts such that no of edges are equal
+
+Fact: degree of the vertices in first part= degree of the vertices in second part
+
+We can use knapsack dp to check if can we divide the vertices into sets such that summation of degree are equal.
+
+https://www.codechef.com/LTIME88A/problems/PERMSPL
+
+*/
